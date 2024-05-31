91480eab5b9bbdeecadbc3da75c6cced3a670d97
Inho <yeoinho75@gmail.com>
2024-05-31 17:24:21 +0900

    feat: Chapeter_3
    - add Anim_Run, Jump
    - add Input mapping

---
+++ Source/Tutorial/MyThirdPersonChar.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyThirdPersonChar.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AMyThirdPersonChar::AMyThirdPersonChar()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	// 컨트롤러 회전할 때 캐릭터는 회전하지 않도록 설정
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	// 캐릭터 무브먼트 설정
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	// 카메라 붐 설정
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	// 캐릭터를 따라다닐 카메라 생성
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+}
+
+// Called when the game starts or when spawned
+void AMyThirdPersonChar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyThirdPersonChar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyThirdPersonChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
