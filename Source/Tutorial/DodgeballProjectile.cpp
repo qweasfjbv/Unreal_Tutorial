9c6fba06c1aa26adac14ccb492c0329911fd68b0
Inho <yeoinho75@gmail.com>
2024-06-15 21:01:13 +0900

    feat: Chapter_6.01
    - Sphere Collision
    - Physical Material

---
+++ Source/Tutorial/DodgeballProjectile.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DodgeballProjectile.h"
+#include "TutorialCharacter.h"
+
+// Sets default values
+ADodgeballProjectile::ADodgeballProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	SphereComponent->SetSphereRadius(35.f);
+	SphereComponent->SetCollisionProfileName(FName("Dodgeball"));
+	SphereComponent->SetSimulatePhysics(true);
+	SphereComponent->SetNotifyRigidBodyCollision(true);
+
+	SphereComponent->OnComponentHit.AddDynamic(this, &ADodgeballProjectile::OnHit);
+
+	// Generally, Collision Component should be Root Component
+	RootComponent = SphereComponent;
+}
+
+// Called when the game starts or when spawned
+void ADodgeballProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADodgeballProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ADodgeballProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) {
+	if (Cast<ATutorialCharacter>(OtherActor) != nullptr) {
+		Destroy();
+	}
+}
